96443fbe0ded0738cd1eb923ae29661258f91099
Dave Hoover <dave.hoover@gmail.com>
2011-03-05 01:23:59 -0600

    Final refactoring before sleep

--- game.rb
+++ game.rb
@@ -3,8 +3,8 @@ require "set"
 def step(living)
   universe = all_cells(living)
   dead = universe - living
-  new_living = dead.select { |cell| alive?(live_neighbor_count(cell, living), false) }
-  new_living += living.select { |cell| alive?(live_neighbor_count(cell, living), true) }
+  new_living = dead.select(&find_living_cells_among_the_dead(living))
+  new_living += living.select(&find_living_cells_among_the_living(living))
   Set.new(new_living)
 end
 
@@ -12,6 +12,18 @@ def all_cells(living)
   living.inject(Set.new) do |universe, cell|
     universe + neighbors_for(cell) + [cell]
   end
+end
+
+def find_living_cells_among_the_dead(living)
+  find_living_cells(living, false)
+end
+
+def find_living_cells_among_the_living(living)
+  find_living_cells(living, true)
+end
+
+def find_living_cells(living, currently_alive)
+  lambda { |cell| alive?(live_neighbor_count(cell, living), currently_alive) }
 end
 
 def alive?(live_neighbor_count, currently_alive)
--- renderer.rb
+++ renderer.rb
@@ -1,18 +1,26 @@
 require "game"
 
 def render(cells, living)
-  sorted_cells = sort(cells)
-  upper_left = sorted_cells.first
-  lower_right = sorted_cells.last
+  vertically_sorted = cells.sort_by {|cell| cell[1]}
+  horizontally_sorted = cells.sort_by {|cell| cell[0]}
+
+  top = vertically_sorted.first
+  bottom = vertically_sorted.last
+  left = horizontally_sorted.first
+  right = horizontally_sorted.last
+  
+  top_left = [left[0], top[1]]
+  bottom_right = [right[0], bottom[1]]
     
-  @original_upper_left ||= upper_left
-  @original_lower_right ||= lower_right
+  @original_top_left ||= top_left
+  @original_bottom_right ||= bottom_right
   
-  lower_right_to_use = sort([lower_right, @original_lower_right]).last
+  bottom_right_to_use = [bottom_right, @original_bottom_right].sort_by {|cell| cell[1]}.last  
+  width = bottom_right_to_use[0] - @original_top_left[0]
+  puts "-" * width
   
-  puts "---"
-  @original_upper_left[1].upto(lower_right_to_use[1]) do |y|
-    @original_upper_left[0].upto(lower_right_to_use[0]) do |x|
+  @original_top_left[1].upto(bottom_right_to_use[1]) do |y|
+    @original_top_left[0].upto(bottom_right_to_use[0]) do |x|
       if living.include?([x, y])
         print "O"
       else
@@ -23,16 +31,11 @@ def render(cells, living)
   end
 end
 
-def sort(cells)
-  cells.sort do |a, b|
-    x_eq = a[0] <=> b[0]
-    x_eq == 0 ? a[1] <=> b[1]  : x_eq
-  end
-end
-
 # living = Set.new([[2,3], [3,3], [4,3]]) # oscillator
 # living = Set.new([[2,2], [3,2], [2,3], [3,3]]) # block
 living = Set.new([[2, 3], [3, 3], [4, 3], [4, 2], [3, 1]]) # glider
+# living = Set.new([[2, 3], [3, 3], [4, 3], [4, 4], [3, 5]]) # inverted glider
+
 until living.empty?
   render(all_cells(living), living)
   living = step(living)
