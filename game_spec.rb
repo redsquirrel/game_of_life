ac13e0e433aab55f630e0cffc10252de1dde1ef3
Dave Hoover <dave.hoover@gmail.com>
2011-03-05 00:03:52 -0600

    Failing spec forcing the big dive

--- game_spec.rb
+++ game_spec.rb
@@ -1,3 +1,16 @@
+def step(living_cells)
+  living_cells
+end
+
 describe "one step" do
+  it "a block will remain a block" do
+    block = [[2,2], [3,2], [2,3], [3,3]]
+    step(block).should == block
+  end
   
+  it "an oscillator will oscillate" do
+    horizontal = [[2,3], [3,3], [4,3]]
+    vertical = [[3,2], [3,3], [3,4]]
+    step(horizontal).should == vertical
+  end
 end
