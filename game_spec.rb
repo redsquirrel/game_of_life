63395dffb726da168dbeed9d90d5fb96c4d0fd26
Dave Hoover <dave.hoover@gmail.com>
2011-03-05 00:23:15 -0600

    Deep dive done; Up for air

--- game_spec.rb
+++ game_spec.rb
@@ -2,20 +2,52 @@ require "set"
 
 def step(living)
   universe = all_cells(living)
-  dead = universe - Set.new(living)
-  new_living = dead.select { |point| alive?(live_neighbor_count(point, living), false) }
-  new_living + living.select { |point| alive?(live_neighbor_count(point, living), true) }
+  dead = universe - living
+  new_living = dead.select { |cell| alive?(live_neighbor_count(cell, living), false) }
+  new_living += living.select { |cell| alive?(live_neighbor_count(cell, living), true) }
+  Set.new(new_living)
 end
+
+def all_cells(living)
+  living.inject(Set.new) do |universe, cell|
+    universe + neighbors_for(cell) + [cell]
+  end
+end
+
+def alive?(live_neighbor_count, currently_alive)
+  if currently_alive
+    live_neighbor_count == 2 || live_neighbor_count == 3
+  else
+    live_neighbor_count == 3
+  end
+end
+
+def live_neighbor_count(cell, living)
+  Set.new(neighbors_for(cell)).intersection(living).size
+end
+
+def neighbors_for(cell)
+  my_x, my_y = cell
+  neighbors = []
+  SHIFTERS.each do |y|
+    SHIFTERS.each do |x|
+      neighbors << [my_x + x, my_y + y]
+    end
+  end
+  neighbors - [cell]
+end
+
+SHIFTERS = [-1, 0, 1]
 
 describe "one step" do
   it "a block will remain a block" do
-    block = [[2,2], [3,2], [2,3], [3,3]]
+    block = Set.new([[2,2], [3,2], [2,3], [3,3]])
     step(block).should == block
   end
   
   it "an oscillator will oscillate" do
-    horizontal = [[2,3], [3,3], [4,3]]
-    vertical = [[3,2], [3,3], [3,4]]
+    horizontal = Set.new([[2,3], [3,3], [4,3]])
+    vertical = Set.new([[3,2], [3,3], [3,4]])
     step(horizontal).should == vertical
   end
 end
